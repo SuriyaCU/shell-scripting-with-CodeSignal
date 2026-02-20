0c04b19108a3a9bf35cea42f20882df1c1d99e43
Suriya C U <suriyac.u.99@gmail.com>
2026-02-20 20:42:18 +0530

    Add check_usage.sh for daily disk usage logging
    Create a shell script to check disk usage and log it daily.

--- UNIT 4 Scheduling Tasks with Cron/check_usage.sh
+++ UNIT 4 Scheduling Tasks with Cron/check_usage.sh
@@ -1,1 +1,12 @@
-create and automate a shell script that checks disk usage daily and appends this information to a log file. You will utilize cron jobs to schedule the script to run automatically every weekday at 9 A.M. Follow the TODO comments to correctly schedule the check_usage.sh file.
+#!/bin/bash
+
+TARGET_DIR="/usercode/FILESYSTEM"
+OUTPUT_FILE=$TARGET_DIR/disk_usage.logs
+
+# TODO: Append the current date to the output file
+
+echo "Disk Usage Statistics" >> $OUTPUT_FILE
+
+# TODO: Append the human readable disk usage statistics of the CWD to the output file (Hint: df)
+
+echo >> $OUTPUT_FILE
